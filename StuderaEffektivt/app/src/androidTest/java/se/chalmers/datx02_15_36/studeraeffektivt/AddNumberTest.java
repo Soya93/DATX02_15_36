adcb330730b7484c9e9ad3778290adc052d932fd
EmmaWestman <eewestman@gmail.com>
2015-01-30 16:25:07 +0100

    simpel test added

---
+++ StuderaEffektivt/app/src/androidTest/java/se/chalmers/datx02_15_36/studeraeffektivt/AddNumberTest.java
@@ -0,0 +1,25 @@
+package se.chalmers.datx02_15_36.studeraeffektivt;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by emmawestman on 15-01-30.
+ */
+public class AddNumberTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testAddNumbers() {
+        int result = AddNumbers.addNumbers(2,6);
+
+        assertEquals(8, result);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
---
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/AddNumbers.java
@@ -0,0 +1,11 @@
+package se.chalmers.datx02_15_36.studeraeffektivt;
+
+/**
+ * Created by emmawestman on 15-01-30.
+ */
+public class AddNumbers {
+
+    public static int addNumbers (int a, int b) {
+        return a + b;
+    }
+}
