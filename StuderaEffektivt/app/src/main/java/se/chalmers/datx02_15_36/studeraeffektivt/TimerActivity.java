65e8d383139bb43ab63821d023b24b4261f5680f
Patricia <chorriballong@gmail.com>
2015-01-30 17:04:16 +0100

    Created TimerActivity and button that leads there

--- StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/TimerActivity.java
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/TimerActivity.java
@@ -1,26 +1,24 @@
 package se.chalmers.datx02_15_36.studeraeffektivt;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class TimerActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_timer);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_timer, menu);
         return true;
     }
 
@@ -37,11 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /** Called when the user clicks the Timer button */
-    public void goToTimer(View view) {
-        Intent intent = new Intent(this, TimerActivity.class);
-        startActivity(intent);
     }
 }
