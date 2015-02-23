8b3ab68c0d546cf2f0bf8838e3adfb93f41dc12a
Sara <sayo.panda.sn@gmail.com>
2015-02-23 01:42:28 +0100

    Merge branch 'timer' into tips

--- StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/TimerActivity.java
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/TimerActivity.java
@@ -1,27 +1,69 @@
 package se.chalmers.datx02_15_36.studeraeffektivt.activity;
 
-import android.content.Intent;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.CountDownTimer;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import se.chalmers.datx02_15_36.studeraeffektivt.R;
 
 
 public class TimerActivity extends ActionBarActivity {
-
+    private String userName;
     private CountDownTimer cdt;
+    private TimePicker t1;
+    private Button startButton;
+    private Button resetButton;
+    private long chosenSeconds;
+    private long seconds;
+    private long timePassed;
+   private DbAccess dbAccess;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        userName = getIntent().getStringExtra("user_Name");
         setContentView(R.layout.activity_timer);
+        instantiate();
+        t1.setIs24HourView(true);
+        t1.clearFocus();
+        setTime();
+        getTimer(5000, 100);
+        DbAccess dbAccess = new DbAccess(this);
 
-        cdt = getTimer(10000, 1000);
+
+
+
+    }
+
+    private void setTime() {
+        t1.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+                                        @Override
+                                        public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                                            chosenSeconds = minute * 60 + hourOfDay * 3600;
+                                            getTimer(chosenSeconds * 1000, 100);
+
+                                        }
+                                    }
+        );
+
+    }
+
+    private void instantiate() {
+        t1 = (TimePicker) findViewById(R.id.timePicker);
+        resetButton = (Button) findViewById(R.id.button_reset);
+        startButton = (Button) findViewById(R.id.button_start_timer);
+
 
     }
 
@@ -48,21 +90,47 @@ public class TimerActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
     /**
      * Set the timer.
      */
-    public CountDownTimer getTimer(long millisInFuture, long countDownInterval){
+    public CountDownTimer getTimer(long millisInFuture, long countDownInterval) {
 
         cdt = new CountDownTimer(millisInFuture, countDownInterval) {
 
             public void onTick(long millisUntilFinished) {
+
                 TextView textView = (TextView) findViewById(R.id.text_timer);
                 textView.setText("seconds remaining: " + millisUntilFinished / 1000);
+                setTimePicker(millisUntilFinished);
+
+                seconds = millisUntilFinished;
+                timePassed += 100;
+            }
+
+            private int secondsToHour(long millisUntilFinished) {
+                return (int) (millisUntilFinished / 1000 / 3600);
+            }
+
+            private int secondsToMin(long millisUntilFinished) {
+                return ((int) ((millisUntilFinished / 1000 % 3600)) + 60) / 60;
+            }
+
+            private void setTimePicker(long millisUntilFinished) {
+                int hour = secondsToHour(millisUntilFinished);
+                int minute = secondsToMin(millisUntilFinished);
+                t1.setCurrentHour(hour);
+                t1.setCurrentMinute(minute);
             }
 
             @Override
             public void onFinish() {
-                //TODO
+                t1.setEnabled(true);
+                setTime();
+                startButton.setText("Start");
+                startButton.setEnabled(true);
+                //dbAccess.insertValue("Alex",Long.toString(timePassed));
+
             }
         };
         return cdt;
@@ -74,12 +142,33 @@ public class TimerActivity extends ActionBarActivity {
      * Called when the user clicks the Start Timer button.
      */
     public void startTimer(View view) {
-        cdt.start();
+        t1.setEnabled(false);
+        if (startButton.getText().equals("Pause")) {
+            startButton.setText("Start");
+            cdt.cancel();
+            getTimer(seconds, 100);
+        } else if (startButton.getText().equals("Start")) {
+            cdt.start();
+            startButton.setText("Pause");
+            resetButton.setEnabled(true);
+
+        }
+    }
+    /*
+    If you hit pause and then reset button and then not choose another time. Timer will count down
+    the seconds that are left.
+    */
+
+    public void resetTimer(View view) {
+        cdt.cancel();
+        startButton.setText("Start");
+        startButton.setEnabled(true);
+        resetButton.setEnabled(false);
+        t1.setEnabled(true);
+        setTime();
     }
 
-    /**
-     * Stop the timer.
-     */
+
 
 
 }
