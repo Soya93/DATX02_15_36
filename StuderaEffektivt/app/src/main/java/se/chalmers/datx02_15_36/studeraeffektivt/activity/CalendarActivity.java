a1597947fe8faa17e39b6b01f054efb0274634e9
EmmaWestman <eewestman@gmail.com>
2015-02-24 15:59:24 +0100

    calendar???

--- StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/CalendarActivity.java
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/CalendarActivity.java
@@ -80,6 +80,20 @@ public class CalendarActivity extends ActionBarActivity {
     private static final int PROJECTION_BEGIN_INDEX = 1;
     private static final int PROJECTION_TITLE_INDEX = 2;
 
+    // Projection array. Creating indices for this array instead of doing
+// dynamic lookups improves performance.
+    public static final String[] EVENT_PROJECTION = new String[] {
+            CalendarContract.Calendars._ID,                           // 0
+            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+    };
+
+    // The indices for the projection array above.
+    private static final int PROJECTION_ID_INDEX = 0;
+    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -164,6 +178,7 @@ public class CalendarActivity extends ActionBarActivity {
         calIntent.putExtra(CalendarContract.Events.TITLE, "TEST!!!!!");
         calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "6207");
         calIntent.putExtra(CalendarContract.Events.DESCRIPTION, "Bra jobbat!!! :)");
+        Log.i("cal", CalendarContract.Calendars.CALENDAR_DISPLAY_NAME);
 
         GregorianCalendar calDate = new GregorianCalendar(year, month, day);
         calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
@@ -181,27 +196,13 @@ public class CalendarActivity extends ActionBarActivity {
     //TODO fix this!
     public void addEventAuto(View view) {
 
+        getCalendars();
+
         TextView textView = (TextView) findViewById(R.id.calendar_text);
-        textView.setText(year + " " + month  + " " + day);
-
-
-        int year = 2015;
-        int month = 2;
-        int day = 10;
-        //TODO get date from phone
-        // add hour and min and sec?
 
 
 
         long calID = 3;
-        long startMillis = 0;
-        long endMillis = 0;
-        Calendar beginTime = Calendar.getInstance();
-        beginTime.set(year, month, day);
-        startMillis = beginTime.getTimeInMillis();
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(year, month, day);
-        endMillis = endTime.getTimeInMillis();
 
         ContentResolver cr = getContentResolver();
         ContentValues values = new ContentValues();
@@ -211,12 +212,14 @@ public class CalendarActivity extends ActionBarActivity {
         values.put(CalendarContract.Events.DESCRIPTION, "TEST");
         values.put(CalendarContract.Events.CALENDAR_ID, calID);
         values.put(CalendarContract.Events.EVENT_TIMEZONE, "Sweden");
-        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+        uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
 
         // get the event ID that is the last element in the Uri
         long eventID = Long.parseLong(uri.getLastPathSegment());
         //
         // ... do something with event ID
+
+        textView.setText(eventID +"");
 
     }
 
@@ -225,6 +228,28 @@ public class CalendarActivity extends ActionBarActivity {
                 .appendQueryParameter(android.provider.CalendarContract.CALLER_IS_SYNCADAPTER,"true")
                 .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_NAME, account)
                 .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_TYPE, accountType).build();
+    }
+
+    public void getCalendars() {
+        // Run query
+
+        cr = getContentResolver();
+        uri = CalendarContract.Calendars.CONTENT_URI;
+        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
+                + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
+                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
+        String[] selectionArgs = new String[] {"eewestman@gmail.com", "com.google",
+                "eewestman@gmail.com"};
+        // Submit the query and get a Cursor object back.
+        cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+
+        int i = 0;
+        while (cur.moveToNext()) {
+
+            Log.d("title: ", cur.getColumnName(i));
+            i++;
+
+        }
     }
 
 }
