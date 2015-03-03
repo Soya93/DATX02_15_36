101d42832c15ba0a71e776d95ab25a5ea15f1572
EmmaWestman <eewestman@gmail.com>
2015-03-03 08:42:15 +0100

    No longer crashes...

--- StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/HomeActivity.java
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/HomeActivity.java
@@ -79,10 +79,10 @@ public class HomeActivity extends Fragment {
             Log.i("HomeActivity: setCalendarInfo", "view:" + view);
 
             //Crashes because view is null...
-            initComponents(view);
-            studySession1_course1.setText(todaysEventsTitles.get(0));
-            studySession2_course1.setText(todaysEventsTitles.get(1));
-            studySession3_course1.setText(todaysEventsTitles.get(2));
+            //initComponents(view);
+            //studySession1_course1.setText(todaysEventsTitles.get(0));
+            //studySession2_course1.setText(todaysEventsTitles.get(1));
+            //studySession3_course1.setText(todaysEventsTitles.get(2));
 
             return todaysEventsTitles;
         }
