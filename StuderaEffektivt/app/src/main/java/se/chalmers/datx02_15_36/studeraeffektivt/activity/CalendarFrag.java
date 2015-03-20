c4885a84d94f586843475dd612e15e13423b2f7a
Sara <sayo.panda.sn@gmail.com>
2015-03-20 18:30:30 +0100

    Refactored main and calendar

--- StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/CalendarFrag.java
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/CalendarFrag.java
@@ -4,19 +4,15 @@ import android.app.AlertDialog;
 
 import android.content.ContentResolver;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import java.util.List;
 
-import se.chalmers.datx02_15_36.studeraeffektivt.IO.LogInHandler;
 import se.chalmers.datx02_15_36.studeraeffektivt.R;
 import se.chalmers.datx02_15_36.studeraeffektivt.model.CalendarModel;
 
@@ -27,29 +23,85 @@ public class CalendarFrag extends Fragment {
 
     CalendarModel calendarModel = new CalendarModel();
     ContentResolver cr;
-    View rootView;
+    View view;
     int week = -1;
+    Button studySession;
+    Button repetition;
+    Button calendar;
     AlertDialog.Builder builder;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-       rootView = inflater.inflate(R.layout.activity_calendar, container, false);
+       view = inflater.inflate(R.layout.activity_calendar, container, false);
        calendarModel = new CalendarModel();
-       return rootView;
+       initComponents();
+       return view;
     }
 
-    public void setContentResolver(ContentResolver cr){
-        this.cr = cr;
+    private void initComponents() {
+        View.OnClickListener myOnlyhandler = new View.OnClickListener() {
+            public void onClick(View v) {
+
+                goToButtonView((Button) v);
+
+            }
+        };
+        studySession = (Button) view.findViewById(R.id.button_add_study_session);
+        studySession.setOnClickListener(myOnlyhandler);
+        repetition = (Button) view.findViewById(R.id.button_add_repetition_session);
+        repetition.setOnClickListener(myOnlyhandler);
+        calendar = (Button) view.findViewById(R.id.button_open_calendar);
+        calendar.setOnClickListener(myOnlyhandler);
     }
 
-    public Intent addStudySession(){
-        return calendarModel.addEventManually(0L, 0L, false, "Studiepass", null, null);
+    private void goToButtonView(Button b) {
+
+        String buttonText = b.getText().toString();
+        switch (buttonText) {
+            case "Lägg till studiepass":
+                this.addStudySession();
+                break;
+            case "Lägg till repetitionspass":
+                this.addRepetitionSession();
+                break;
+            case "Öppna kalendern":
+                this.openCalendar();
+                break;
+
+        }
     }
 
-    public Intent openCalendar(){
-        return calendarModel.openCalendar();
+    private void addStudySession(){
+        this.getActivity().startActivity(calendarModel.addEventManually(0L, 0L, false, "Studiepass", null, null));
+    }
+
+    private void openCalendar(){
+        this.getActivity().startActivity(calendarModel.openCalendar());
+    }
+
+    private void addRepetitionSession() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        //the alternatives
+        String[] alternatives = {"LV1", "LV2", "LV3", "LV4", "LV5", "LV6", "LV7", "LV8"};
+
+        builder.setTitle("Välj ett pass att repetera")
+                .setItems(alternatives, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //TODO hämta några random uppgifter
+                        String tasks = "";
+                        int studyWeek = which + 1;
+                        startActivity(calendarModel.addEventManually(0L, 0L, true, "Repititonspass för LV" + studyWeek, null, "Repetera " + tasks));
+
+                        // The 'which' argument contains the index position
+                        // of the selected item
+                    }
+                });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     public List<String> getTodaysEvents() {
@@ -60,62 +112,7 @@ public class CalendarFrag extends Fragment {
         return calendarModel.readEventsSunday(cr);
     }
 
-
-    public CalendarModel getCalendarModel(){
-        return this.calendarModel;
-    }
-
-    //Används inte men kommer vara användbart
-    public void readCalendar() {
-        calendarModel.getCalendars(cr, "sayo.panda.sn@gmail.com", "com.google");
-
-        calendarModel.readEvents(cr, 0L, 0L);
-    }
-
-    public void addEventAuto(View view) {
-        calendarModel.getCalendars(cr, "sayo.panda.sn@gmail.com", "com.google");
-        calendarModel.getCalendars(cr, "eewestman@gmail.com", "com.google");
-        Long eventID = this.calendarModel.addEventAuto(cr);;
-    }
-
-    /*  Whatever is below this line does not work and is not invoked */
-    public void createBuilder(){
-        builder.create().show();
-    }
-
-    public void openDialog(){
-       builder = new AlertDialog.Builder(getActivity());
-
-       //the alternatives
-       String [] alternatives = {"LV1", "LV2", "LV3", "LV4", "LV5", "LV6", "LV7", "LV8"};
-       String tasks = "";
-
-       builder.setTitle("Välj ett pass att repetera")
-               .setItems(alternatives, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int which) {
-                       int studyWeek = which+1;
-                       setWeek(studyWeek);
-                      Log.i("weekInOpen", week + "");
-                       // The 'which' argument contains the index position
-                       // of the selected item
-                   }
-               });
-        //builder.create().show();
-   }
-
-    public Intent addRepetition() {
-        return calendarModel.addEventManually(0L, 0L, true, "Repititonspass för LV" + week, null, "Repetera " +  "");
-    }
-
-    private void setWeek(int week){
-        this.week = week;
-    }
-
-    public int getWeek(){
-        return this.week;
-    }
-
-    public AlertDialog.Builder getBuilder(){
-        return builder;
+    public void setContentResolver(ContentResolver cr){
+        this.cr = cr;
     }
 }
