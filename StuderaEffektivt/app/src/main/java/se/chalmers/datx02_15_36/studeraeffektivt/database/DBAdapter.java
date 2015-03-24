a289fb9fbde0dff446baf42979bd399df462b496
Patricia <chorriballong@gmail.com>
2015-03-24 14:10:39 +0100

    Database skeleton works, Courses only & list updates

--- StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/database/DBAdapter.java
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/database/DBAdapter.java
@@ -2,10 +2,19 @@ package se.chalmers.datx02_15_36.studeraeffektivt.database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import se.chalmers.datx02_15_36.studeraeffektivt.model.Course;
 
 /**
+ * A database adapter that is used to read and write to/from the database.
+ * The database currently has one table for Courses with coursecode and coursename.
  * Created by Patricia on 2015-03-11.
  */
 public class DBAdapter  {
@@ -14,18 +23,38 @@ public class DBAdapter  {
 
     public DBAdapter(Context context){
         dbHelper = new DBHelper(context);
+        Log.i("DB", "DBAdapter created.");
     }
 
-    public void insertCourse(String courseCode, String courseName){
+    /**
+     * Insert a Course to the database.
+     * @param courseCode
+     * @param courseName
+     * @return
+     */
+    public long insertCourse(String courseCode, String courseName){
+
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        //insert things...
+        cv.put("_coursecode", courseCode);
+        cv.put("name", courseName);
+        return db.insert(dbHelper.TABLE_COURSES, null, cv);
     }
 
+    /**
+     * Get all courses.
+     */
+    public Cursor getCourses(){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        return db.query(dbHelper.TABLE_COURSES, null, null, null, null, null, null);
+    }
 
+    /*The DBHelper class*/
     public static class DBHelper extends SQLiteOpenHelper{
         private static final String DATABASE_NAME = "pluggapp.db";
         private static final int DATABASE_VERSION = 1; //Has to be incremented to update.
+
+        private static final String TABLE_COURSES = "COURSES";
 
 
         public DBHelper(Context context){
@@ -36,7 +65,8 @@ public class DBAdapter  {
         public void onCreate(SQLiteDatabase db) {
             //Called when database is created.
             //Creation of schemas and initial insert of data.
-            db.execSQL("CREATE TABLE COURSES (_coursecode VARCHAR(50) PRIMARY KEY, Name VARCHAR(50))");
+            db.execSQL("CREATE TABLE COURSES (_coursecode VARCHAR(50) PRIMARY KEY, name VARCHAR(50))");
+
         }
 
         @Override
