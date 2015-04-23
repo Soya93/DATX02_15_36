4a14d67e70103be27e93e8ee6235ce1297761b0d
alexandraback <alexandra1.back@gmail.com>
2015-04-23 13:22:39 +0200

    changed xml for timer

--- StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/TimerSettingsActivity.java
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/TimerSettingsActivity.java
@@ -5,46 +5,63 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 import se.chalmers.datx02_15_36.studeraeffektivt.R;
+import se.chalmers.datx02_15_36.studeraeffektivt.model.Child;
+import se.chalmers.datx02_15_36.studeraeffektivt.model.ExpandListAdapter;
+import se.chalmers.datx02_15_36.studeraeffektivt.model.Group;
 
 /**
  * Created by alexandraback on 21/04/15.
  */
 public class TimerSettingsActivity extends ActionBarActivity {
-    private ExpandableListView listView;
+    private ExpandListAdapter ExpAdapter;
+    private ArrayList<Group> ExpListItems;
+    private ExpandableListView ListView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timersettings);
-        listView = (ExpandableListView) findViewById(R.id.expandableListView);
-        String[] listValues = new String[]{
-                "Antal repetitioner", "Pluggfas", "Vilofas"
-        };
+        ListView = (ExpandableListView) findViewById(R.id.expandableListView);
 
-        /*
-        ExpandableListAdapter adapter = new ExpandableListAdapter(this,listValues);
-        listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
 
-                // ListView Clicked item index
-                int itemPosition     = position;
 
-                // ListView Clicked item value
-                String  itemValue    = (String) listView.getItemAtPosition(position);
+    }
 
-                // Show Alert
 
+    public ArrayList<Group> SetStandardGroups() {
+
+        String group_names[] = { "GROUP A", "GROUP B", "GROUP C", "GROUP D",
+                "GROUP E", "GROUP F", "GROUP G", "GROUP H" };
+
+        ArrayList<Group> list = new ArrayList<Group>();
+
+        ArrayList<Child> ch_list;
+
+        int size = 4;
+        int j = 0;
+
+        for (String group_name : group_names) {
+            Group gru = new Group();
+            gru.setName(group_name);
+
+            ch_list = new ArrayList<Child>();
+            for (; j < size; j++) {
+                Child ch = new Child();
+                ch_list.add(ch);
             }
-        });*/
+            gru.setItems(ch_list);
+            list.add(gru);
 
+            size = size + 4;
+        }
+
+        return list;
     }
 }
