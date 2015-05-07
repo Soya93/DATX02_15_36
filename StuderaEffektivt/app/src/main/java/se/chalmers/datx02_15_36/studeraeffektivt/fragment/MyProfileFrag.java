049b97496f5b5cf0af753590caa60433155a3c0f
Patricia <chorriballong@gmail.com>
2015-05-07 14:45:50 +0200

    New icons for the profile page

--- StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/fragment/MyProfileFrag.java
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/fragment/MyProfileFrag.java
@@ -4,6 +4,9 @@ package se.chalmers.datx02_15_36.studeraeffektivt.fragment;
     import android.content.DialogInterface;
     import android.content.Intent;
     import android.database.Cursor;
+    import android.graphics.Color;
+    import android.graphics.PorterDuff;
+    import android.graphics.drawable.Drawable;
     import android.net.Uri;
     import android.os.Bundle;
     import android.support.v4.app.Fragment;
@@ -16,9 +19,12 @@ package se.chalmers.datx02_15_36.studeraeffektivt.fragment;
     import android.widget.AdapterView;
     import android.widget.Button;
     import android.widget.EditText;
+    import android.widget.ImageView;
     import android.widget.ListView;
     import android.widget.SimpleAdapter;
     import android.widget.Toast;
+
+    import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
     import java.util.ArrayList;
     import java.util.HashMap;
@@ -32,6 +38,7 @@ package se.chalmers.datx02_15_36.studeraeffektivt.fragment;
     import se.chalmers.datx02_15_36.studeraeffektivt.activity.TechsNTipsActivity;
     import se.chalmers.datx02_15_36.studeraeffektivt.database.DBAdapter;
     import se.chalmers.datx02_15_36.studeraeffektivt.model.Course;
+    import se.chalmers.datx02_15_36.studeraeffektivt.util.Constants;
 
 public class MyProfileFrag extends Fragment {
     private static final String ARG_PARAM1 = "param1";
