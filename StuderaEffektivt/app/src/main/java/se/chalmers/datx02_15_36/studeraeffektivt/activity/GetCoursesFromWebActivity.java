511d358ff1d7e59729cb4a2443b64d63e1d46f03
Patricia <patriciapaulsson@gmail.com>
2015-07-03 15:05:33 +0200

    Git weirdness

--- StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/GetCoursesFromWebActivity.java
+++ StuderaEffektivt/app/src/main/java/se/chalmers/datx02_15_36/studeraeffektivt/activity/GetCoursesFromWebActivity.java
@@ -71,6 +71,7 @@ public class GetCoursesFromWebActivity extends ActionBarActivity {
         return true;
     }
 
+    //YOLOOOO
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle bar item clicks here. The action bar will
